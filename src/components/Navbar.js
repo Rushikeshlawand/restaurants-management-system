5576c7a73f9986152b4f025306d0f78b93d7130c
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-08-31 00:36:25 +0530

    navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import '../styles/Navbar.css'; // Add styling here
+import '../styles/Navbar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <ul className="nav-links">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#menu-card">Menu Card</a></li>
-                <li><a href="#my-order">My Order</a></li>
-                <li><a href="#testimonials">Testimonials</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
+            <a href="#home">Home</a>
+            <a href="#menu">Menu Card</a>
+            <a href="#orders">My Order</a>
+            <a href="#testimonials">Testimonials</a>
+            <a href="#contact">Contact</a>
         </nav>
     );
 };
