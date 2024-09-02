248e2156c6e8759fe746edd8f50d53394a9c6c35
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-09-03 00:33:42 +0530

    navbar updated

--- src/components/CustomerPortal/Navbar/Navbar.js
+++ src/components/CustomerPortal/Navbar/Navbar.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // Corrected import path
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faBars, faShoppingCart, faStar, faPhone } from '@fortawesome/free-solid-svg-icons';
+
+import './Navbar.css'; 
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <Link to="/">Home</Link>
-            <Link to="/menu">Menu Card</Link>
-            <Link to="/myorder">My Order</Link>
-            <Link to="/testimonials">Testimonials</Link>
-            <Link to="/contact">Contact</Link>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="./components/Images/logo.png" alt="Restaurant Logo" className="logo-image" />
+      </div>
+      <div className="navbar-links">
+        <Link to="/"><FontAwesomeIcon icon={faHome} className="navbar-icon" /> Home</Link>
+        <Link to="/menu"><FontAwesomeIcon icon={faBars} className="navbar-icon" /> Menu Card</Link>
+        <Link to="/myorder"><FontAwesomeIcon icon={faShoppingCart} className="navbar-icon" /> My Order</Link>
+        <Link to="/testimonials"><FontAwesomeIcon icon={faStar} className="navbar-icon" /> Testimonials</Link>
+        <Link to="/contact"><FontAwesomeIcon icon={faPhone} className="navbar-icon" /> Contact</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
